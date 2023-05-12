169390d09e8d5acd737a9e0d0ef009bb520bb65e
dugabrielle <gabduarte113@gmail.com>
2023-05-12 16:39:12 -0300

    concluído

---
+++ exercício1/script.js
@@ -0,0 +1,7 @@
+
+function resultadoTabuada(){
+    let num1 = parseInt(document.getElementById("primeiroNumero").value);
+    let num2 = parseInt(document.getElementById("segundoNumero").value);
+    document.getElementById("resultado").innerHTML = num1 * num2;
+}
+
