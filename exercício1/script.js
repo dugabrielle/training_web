ac528a0145bfccb0f9838a5f7459fb1f32c12cac
dugabrielle <dugabrielle9@gmail.com>
2023-09-26 20:35:27 -0300

    add textContent

--- exercício1/script.js
+++ exercício1/script.js
@@ -2,6 +2,6 @@
 function resultadoTabuada(){
     let num1 = parseInt(document.getElementById("primeiroNumero").value);
     let num2 = parseInt(document.getElementById("segundoNumero").value);
-    document.getElementById("resultado").innerHTML = num1 * num2;
+    document.getElementById("resultado").textContent = num1 * num2;
 }
 
