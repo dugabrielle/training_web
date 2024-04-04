0fee03f12ee72cf8780328e1c93e5974f547353a
Gabi <dugabrielle9@gmail.com>
2024-04-04 17:02:13 -0300

    add classList

--- exercício6/script.js
+++ exercício6/script.js
@@ -6,11 +6,13 @@ function executar() {
 
     let box = document.querySelector('.box');
 
-    if (box.style.backgroundColor === 'darkcyan') {
-        box.style.backgroundColor = 'darkorchid';
+    if (box.classList.contains ('cor-1')) {
+        box.classList.remove('cor-1');
+        
     } else {
-        box.style.backgroundColor = 'darkcyan';
-    }
+ 
+        box.classList.add('cor-1');
+    } 
 }
 
 btn.addEventListener('click', executar);
